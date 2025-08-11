b852d4ca91b6c30ac61b77e483d46c231c2024a8
Saidazam Rasulhujaev <rasulhuzaevsaid@gmail.com>
2025-08-11 20:09:59 +0500

    fix: shit

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,35 +1,25 @@
 import {
-    ApolloClient,
-    InMemoryCache,
-    createHttpLink
-  } from "@apollo/client";
-  import { setContext } from "@apollo/client/link/context";
-  
-  const httpLink = createHttpLink({
-    uri: "https://api.drinkmall.ru/graphql/client",
-  });
-  
-  const authLink = setContext((_, { headers }) => {
-    return {
-      headers: {
-        ...headers,
-        auth: "98T7hcAUQE",
-      },
-    };
-  });
-  
-  export const client = new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache(),
-  });
-  
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
-//   const authLink = setContext((_, { headers }) => {
-//     const initData = window.Telegram?.WebApp?.initData ?? "";
-//     return {
-//       headers: {
-//         ...headers,
-//         auth: initData,
-//       },
-//     };
-//   });
+const httpLink = createHttpLink({
+  uri: "https://api.drinkmall.ru/graphql/client",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const initData = window.Telegram?.WebApp?.initData ?? "";
+  return {
+    headers: {
+      ...headers,
+      auth: initData,
+    },
+  };
+});
+
+export const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
